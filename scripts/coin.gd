0b91630ecb9ceed6c607e405c9811db666683af8
qsm <qasimsuhail1@gmail.com>
2026-02-15 12:15:26 +0500

    update

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,11 +1,6 @@
 extends Area2D
 
-# 1. Note: We use 'area', not 'body', because it's an Area2D collision
 func _on_area_entered(area):
-	# 2. 'area' is the "hitbox" node. We get its PARENT to find the "player" node.
-	var parent_node = area.get_parent()
-	
-	# 3. Now checking the parent (which is the Node2D named 'player')
-	if parent_node.name == "player":
+	if area.get_parent().name == "player":
 		GameManager.add_score(1)
 		queue_free()
